94c83d43d426299195dd633bbef4fd3228684138
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-04-01 18:21:13 -0300

    Add files via upload

---
+++ Funções JS/cidadaoPrimeiraClasse.js
@@ -0,0 +1,46 @@
+//Cidadão de primeira linha.
+//Função é um algoritmo.
+//Recebe parâmetros e retorna um determinado valor.
+//Função é tratado como dado.
+//First-Class Object(Citizens)
+
+//Diversas situações em que a função pode ser usada:
+
+//Forma Literal:
+
+function fun1() {}
+
+//Armazenar em uma variável:
+
+const fun2 = function() {}
+
+//Armazenar em um array:
+
+const array = [function (a,b) {return a + b}, fun1, fun2]
+console.log(array[0](2,3))
+
+//Armazenar em um atributo de objeto:
+
+const obj = {}
+obj.falar = function () {return 'Opa'}
+console.log(obj.falar())
+
+//Passar função como parâmetro:
+
+function run(fun) {
+    fun()
+}
+
+run(function() {console.log('Executando...')})
+
+//Uma função pode retornar/conter uma função:
+
+function soma (a, b) {
+    return function (c) {
+        console.log(a + b + c)
+    }
+}
+
+soma (2,3)(4)
+const cincoMais = soma(2,3)
+cincoMais(4)
