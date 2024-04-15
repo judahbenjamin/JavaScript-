d5919a52aaebe29d743744bf0765521579f92da1
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-04-15 15:41:22 -0300

    Update parametrosEretornosSaoOpcionais.js

--- Funções JS/parametrosEretornosSaoOpcionais.js
+++ Funções JS/parametrosEretornosSaoOpcionais.js
@@ -13,4 +13,6 @@ console.log(area())
 console.log(area(2,3,15,73,11))
 console.log(area(5,5))
 
-//O JS pode retornar qualquer tipo de dado.
+//Nesse exemplo, em um determinado momento a função pode retornar um valor, e 
+//em um outro momento não. Esse exemplo é válido pro JS.
+//O JS pode retornar qualquer tipo de dado. Isso o torna uma linguagem muito flexível.
